f68162b385713ad1165f108c7dec4720d4d20405
Loïc Dalloz <osblouf@gmail.com>
2014-08-05 11:02:38 +0200

    Update main program

--- TL.py
+++ TL.py
@@ -6,7 +6,7 @@ sys.path.insert(0, 'src/')
 from configs import Config_manager 
 
 # Stating point of the progtramm
-
+# Get the parameters
 configs = Config_manager()
-
+#store them in the configs manager
 configs.parse()
