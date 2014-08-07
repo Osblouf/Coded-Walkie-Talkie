3fc0b18c4afc9ccb7fb174913bc323e65afe2ded
Loïc <osblouf@gmail.com>
2014-08-07 11:47:52 +0200

    multi client added in example

--- Doc/Test/multi-server/app.py
+++ Doc/Test/multi-server/app.py
@@ -1,5 +1,37 @@
 # This example shows how we use the network
 
 # Last modification : Loic
+from net_class import Net_manager
 
-import socket
+def r_func(data):
+	print 'Received : ', data
+
+not_get_all = True
+ip_list = list()
+
+while not_get_all:
+	ip = raw_input()
+	if ip == '':
+		not_get_all = False
+
+	else:
+		ip_list.append(ip)
+
+net = Net_manager(15000, r_func)
+for ip in ip_list:
+	net.Connect_to(ip)
+
+net.Start_listening()
+	
+not_get_all = True
+while not_get_all:
+	message = raw_input()
+
+	if message == 'stop':
+		not_get_all = False
+	else:
+		net.Send_to_all(message)
+
+net.Close()
+
+
--- Doc/Test/multi-server/net_class.py
+++ Doc/Test/multi-server/net_class.py
@@ -4,36 +4,100 @@
 import threading
 import socket
 
-class Net_manage:
+class Net_manager:
 
 	# List of all client
-	clients = []
-	server_thread
+	global clients
+	global receive_func
+	global clients_threads
+	global run
+	global server_thread
 
 	# Constructor :
-	def __init__(self, port):
+	def __init__(self, port, received_function):
 		self.port = port
+		Net_manager.receive_func = received_function
+		Net_manager.run = True
+		Net_manager.clients = list()
+		Net_manager.clients_threads = list()
 		
 	# Connect to an other device
 	def Connect_to(self, ip_addr):
 		s = socket.socket()
 		try:
+			print 'Try connecting to : ', ip_addr
 			s.connect((ip_addr, self.port))
-			clients.append(s)
+			Net_manager.clients.append(s)
+			th = threading.Thread(None, 
+					Net_manager.Client_thread_func,
+					None, 
+					(s,), 
+					None)
+			th.daemon = True
+			th.start()
+			Net_Manager.clients_threads.append(th)
+			print 'Connection OK.'
 		except Exception, e:
 			print "Error while connection to ", ip_addr, ".\n\t", e		
 
 	# Start the server to listen all incoming connections
 	def Start_listening(self):
-		server_thread = threading.Thread(None, Thread_func, None, None, None)
+		print 'Start listening...'
+		Net_manager.server_thread = threading.Thread(None, Net_manager.Listening_thread_func, None, (self.port,), None)
+		Net_manager.server_thread.daemon = True
+		Net_manager.server_thread.start()
+		print 'Listening ok.'
 
 	# Send to all
 	def Send_to_all(self, data):
-	
-	# Thread function
-	@staticmethod
-	def Thread_func():
+		print 'Send to all...'
+		for sock in Net_manager.clients:
+			print '.'
+			sock.send(data)
+		print 'Send OK.'
 
-	# Close all threads
+	# Listening thread function
+	@staticmethod
+	def Listening_thread_func(port):
+		s = socket.socket()
+		s.bind((socket.gethostname(), port))
+		s.listen(5)
+
+		while Net_manager.run:
+			client, address = s.accept()
+			print 'Incoming connection from : ', address
+			Net_manager.clients.append(client)
+			th = threading.Thread(None, 
+					Net_manager.Client_thread_func,
+					None,
+					(client,),
+					None)
+			th.daemon = True
+			th.start()
+			Net_manager.clients_threads.append(th)
+			print 'Connection OK'
+
+
+	# Client thread function
+	@staticmethod
+	def Client_thread_func(sock):
+		while Net_manager.run:
+			data = sock.recv()
+			print '(data received)'
+			Net_manager.receive_func(data)
+		
+	# Close all threads and sockets
 	def Close(self):
+		print 'Close everything...'
+		Net_manager.run = False
+		if len(Net_manager.clients) != 0:
+			for sock in Net_manager.clients:
+				sock.close()
+		
+		#if len(Net_manager.clients_threads) != 0:
+		#	for th in Net_manager.clients_threads:
+		#		th._stop()
+		#Net_manager.server_thread._stop()
+
+		print 'Closing OK.'
 
