0f279083be5579f6af27ff81351a49507968e5cd
Loïc <osblouf@gmail.com>
2014-08-07 16:58:23 +0200

    Update network manager

--- src/network.py
+++ src/network.py
@@ -2,31 +2,87 @@
 # This module will be in charge of the network 
 # Last modification : Loic 
 
+import select
+import socket
+import sys
+import signal
+
 class network_manager:
-	#This module manage the network in the project coded walkie talkie.
+	# Class arguments :
+	#	- self.port		: port used
+	#	- self.proc_func	: function used to process data received
+	#	- self.clients		: socket list of connections
+	#	- self.server_socket	: listening server socket
+	# Constructeur
+	def __init__(self, port, process_function):
+		self.port = port
+		self.proc_func = process_function
+		self.clients = []
 
-	#This module's fields are listed below:
-	# - n_mode : mode of connection used to connect the device
-
-
-	#Start with initiate the mode you want :
-	#- 'normal' will use the normal use of network like sockets, one route, etc
-
-	
-	#start the network module with this method
-	#Start with initiate the mode you want :
-	#- 'normal' will use the normal use of network like sockets, one route, etc
-	def __init__(self):
-
-	# Read from the network network return the new data
-	def Get_data(self):
-
-	# send something on the network
-	def Send(self, data):
-
-	# Process the magic of network coding
-	def Process_magic(self):
+	# Connect to a server
+	def Connect_to(self, ip_addr):
+		try:
+			print 'Connect to ', ip_addr
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip_addr, self.port))
+			self.clients.append(s)
+			print 'Connection OK'
+		except Exception, e:
+			print 'Exception while connecting to :', ip_addr, '\n\t', e
 
 	# Start the server
-	def Start_listening(self):
-		
+	def Start_listenning(self):
+		try:
+			print 'Start listenning on port ', self.port
+			self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			self.server_socket.bind(('', port))
+			self.server.listen(5)
+			print 'Listening OK'
+		except Exception, e:
+			print 'Error while trying to listen :\n\t', e
+			
+
+	# Process the network magic (each loop tic)
+	def Network_magic(self):
+		try:
+			inputready, outputready, exceptready = select.select(self.server_socket, [], [])
+		except select.error, e:
+			print 'FATAL SELECT ERROR !'
+			exit()
+		except socket.error, e:
+			print 'FATAL SOCKET ERROR !'
+			exit()
+
+		for s in inputready:
+			# New connection
+			if s == self.server_socket:
+				client, addr = self.server_socket.accept()
+				print 'New connection from ', addr
+				self.clients.append(client)
+			# Incoming data
+			else:
+				try:
+					data = server_socket.recv(1024)
+					if data:
+						self.proc_func(data)
+					else:
+						print 'Connection lost !'
+						self.clients.remove(s)
+				except socket.error, e:
+					print 'Error while reading input data !'
+
+	# Send to all clients
+	def Send_to_all(self, message):
+		for s in self.clients:
+			print 'Sending data.'
+			s.send(message)
+
+	# Close everything properly
+	def Close(self):
+		for s in self.clients:
+			s.close
+			
+		self.server_socket.close()
+
+
