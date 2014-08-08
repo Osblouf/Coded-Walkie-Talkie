7f2538b4faff9695f5c4d7d9b77f67aa03434d33
Loïc <osblouf@gmail.com>
2014-08-08 11:23:07 +0200

    Example network_manager added

---
+++ Doc/client_server_audio_stream/client.py.REMOTE.5307.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import pyaudio
+import socket
+import sys
+import time
+
+# Pyaudio Initialization
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 10240
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format = FORMAT,
+                channels = CHANNELS,
+                rate = RATE,
+                input = True,
+                frames_per_buffer = chunk)
+
+audioInputMode = False # manages the input mode : streaming sound input 
+
+# Socket Initialization
+
+host = '192.168.2.224'
+#host = socket.gethostname()
+port = 12345
+size = 1024
+s = socket.socket()
+
+# Error management
+try:
+	s.connect((host,port))
+except Exception, e:
+	print 'An error cured while connecting !\n\t', e
+	print '\n Exit \n'
+	exit()
+
+print "Client is connected to : ", host
+
+# Main Functionality
+while 15:
+    entry = sys.stdin.read(1)
+    if entry == "s" :
+	audioInputMode = not audioInputMode
+	print "Switching mode to : ", audioInputMode
+
+    if audioInputMode == True :
+	print "sending mode :"
+    	data = stream.read(chunk)
+	try:
+    		s.send(data)
+	except Exception, e:
+		print 'Error while sendig data !\n\t%s\nExit\n', `e`
+		exit()
+
+	try:
+    		s.recv(size)
+	except Exception, e:
+		print 'Error while receiving data !\n\t%s\nExit\n', `e`
+
+    if audioInputMode == False :
+	print "receiving mode : "
+	#inp = s.recv(chunk)
+    	#if inp :
+	    # treatement here 
+	#    print " in mode reading : %s", inp
+
+s.close()
+stream.close()
+p.terminate()
